f56cf77fdc79f4e8cdae11e8ef36b44f0b17cefc
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2023-09-22 21:41:07 +0300

    Updating DIO Driver

--- MCAL/DIO/LIB/BIT_MATH.h
+++ MCAL/DIO/LIB/BIT_MATH.h
@@ -1,19 +1,16 @@
 /*****************************************************/
 /*****************************************************/
 /**************   Author: Doaa Tawfik  ***************/
-/**************   Layer: MCAL          ***************/
-/**************   SWC: Port            ***************/
+/**************   Layer: LIBRARY       ***************/
 /**************   Version: 1.00        ***************/
 /*****************************************************/
 /*****************************************************/
-#ifndef PORT_INTERFACE_H_
-#define PORT_INTERFACE_H_
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
 
-
-void PORT_voidInit(void);
-
-
-
-
+#define SET_BIT(VAR , BIT_NUM) (VAR|=(1<<BIT_NUM))
+#define CLEAR_BIT(VAR , BIT_NUM) (VAR&=(~(1<<BIT_NUM)))
+#define TOGGLE_BIT(VAR , BIT_NUM) (VAR^=(1<<BIT_NUM))
+#define GET_BIT(VAR , BIT_NUM) (1&(VAR>>BIT_NUM))
 
 #endif
